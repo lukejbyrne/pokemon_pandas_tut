0f5cb11eb22ca84b99e3677fee6be6925f8c8cb2
Luke Byrne <lukebyrnee97@gmail.com>
2024-01-18 12:54:57 +0000

    create new column in df

--- modifying_data.py
+++ modifying_data.py
@@ -3,3 +3,8 @@ import pandas as pd
 ### Import Data
 # df = dataframe
 df = pd.read_csv('pokemon_data.csv')
+
+# Create new column in data frame
+df['Total'] = df['HP'] + df['Attack'] + df['Defense']
+
+print(df)
