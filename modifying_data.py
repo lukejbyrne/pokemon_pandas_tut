77cd95a9ef469f657028966ed69ccecc7168ad8a
Luke Byrne <lukebyrnee97@gmail.com>
2024-01-18 13:24:32 +0000

    re-arrange cols

--- modifying_data.py
+++ modifying_data.py
@@ -5,9 +5,15 @@ import pandas as pd
 df = pd.read_csv('pokemon_data.csv')
 
 # Create new column in data frame
-df['Total'] = df['HP'] + df['Attack'] + df['Defense']
+df['Total1'] = df['HP'] + df['Attack'] + df['Defense']
+df['Total2'] = df.iloc[:, 4:10].sum(axis=1) # Integer location, where : = all rows, axis = 1 sums horizontally
 
 # Drop a column
-df = df.drop(columns=['Total'])
+df = df.drop(columns=['Total1'])
 
-print(df)
+# Move col Total2
+cols = list(df.columns)
+new_cols = cols[0:4] + [cols[-1]] + cols[4:12]
+df = df[new_cols]
+
+print(df.head(5))
