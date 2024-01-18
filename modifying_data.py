021ad74c7cdbaf8de565dba89a2491d21952c449
Luke Byrne <lukebyrnee97@gmail.com>
2024-01-18 12:58:36 +0000

    drop a column e.g.

--- modifying_data.py
+++ modifying_data.py
@@ -7,4 +7,7 @@ df = pd.read_csv('pokemon_data.csv')
 # Create new column in data frame
 df['Total'] = df['HP'] + df['Attack'] + df['Defense']
 
+# Drop a column
+df = df.drop(columns=['Total'])
+
 print(df)
